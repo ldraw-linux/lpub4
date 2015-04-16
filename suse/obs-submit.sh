dac0ee4008e75cd69423d8074356485c9e03515a
Milan Vancura <mvancura@suse.cz>
2015-04-16 17:47:26 +0200

    suse/obs-submit.sh: simplify the code

--- suse/obs-submit.sh
+++ suse/obs-submit.sh
@@ -2,53 +2,63 @@
 PRJ="$1"
 PKG=lpub4
 
-if [[ -z $1 ]] ; then
+function die() {
+	echo "ERROR: $1" >&2
+	exit 1
+}
+
+function warn() {
+	echo "WARNING: $1" >&2
+}
+
+#
+# Check options and the commit the user wants to make a package from
+#
+if [[ -z $1 ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]] ; then
 	echo "Usage: $0 OBS_PROJECT_NAME [build|force]" >&2
 	exit 1
 fi
 
-
 BUILD=false
 FORCE=false
-[[ x$2 == xforce ]] && FORCE=true
-[[ x$2 == xbuild ]] && {
+[[ "$2" == "force" ]] && FORCE=true
+[[ "$2" == "build" ]] && {
 	BUILD=true
 	FORCE=true
 }
 
-
 if git status --porcelain |grep -q M; then
-	echo "Uncommited changes:"
-	git status --short
+	warn "Uncommited changes:"
+	git status --short >&2
 	if git status --porcelain .. |grep -q M && ! $FORCE; then
-		echo "Please commit your changes to git first. To override, run:" >&2
-		echo "$0 $1 force" >&2
-		exit 1
+		die "Please commit your changes to git first. To override, run: $0 $1 force"
 	fi
 fi
 
 TAG=`git tag --points-at HEAD`
 if [[ -z $TAG ]]; then 
-	echo "Not on a tagged commit."
+	warn "Not on a tagged commit."
 	TAG=`git describe --abbrev=0`
 	[[ -z $TAG ]] && exit 1
 
 	if ! $FORCE; then
-		echo "To use the most recent tag ($TAG) as the version string, run:" >&2
-		echo "$0 $1 force" >&2
-		exit 1
+		die "To use the most recent tag ($TAG) as the version string, run: $0 $1 force"
 	fi
 fi
 echo "Using $TAG as version string."	
 
 
+#
+# Create the directory of package sources
+#
 D=`mktemp -d`
+
 
 pushd $D
 if ! osc co "$PRJ" "$PKG"; then
 	popd
 	rm -rf $D
-	exit 1
+	die "Cannot check the project out from BS."
 fi
 
 PRJD="$D/$PRJ/$PKG"
@@ -56,7 +66,7 @@ PRJD="$D/$PRJ/$PKG"
 if ! cd $PRJD; then 
 	popd
 	rm -rf $D
-	exit 1
+	die "Cannot get into the package directory."
 fi
 rm -rf *
 
@@ -69,8 +79,6 @@ pushd ..
 git archive --prefix=$PKG/ HEAD | bzip2 > $PRJD/$PKG.tar.bz2
 popd
 HASH=`git log -1 --pretty="format:%H"`
-
-
 
 pushd $PRJD
 if $BUILD; then
